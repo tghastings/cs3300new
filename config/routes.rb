e1cd45a50a13b7893725ad10a4b581cbf9b705c3
Tom <thomas@thomashastings.com>
2021-04-19 23:17:08 +0000

    Changes to be committed: 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "projects#index"
   resources :projects
+  #just for fun
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
